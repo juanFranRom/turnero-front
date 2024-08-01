028e34c786f5a364d62f69b7b318131f0d6fa93c
juanFranRom <romanoj.francisco@gmail.com>
2024-08-01 11:07:17 -0300

    Fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google" content="notranslate"/>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
